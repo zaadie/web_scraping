8ac9a75f0beb4f07aceb8670f5a9ba60c307ba16
zaadie <saadiyafourie@gmail.com>
2019-04-01 14:54:32 +1100

    First Python Webscrape Initiated

---
+++ firstwebscrapeever.py
@@ -0,0 +1,35 @@
+from urllib import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+my_url = 'https://www.newegg.com/global/au-en/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=gpu&N=-1&isNodeId=1'
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# grabs each product
+containers = page_soup.findAll("div", {"class" : "item-container"})
+
+# container = containers[0].img["title"]
+# container = containers[0]
+
+# brand = page_soup.findAll("div", {"class" : "item-info"})
+# Check amount len(brand)
+
+# Only works from index 4+
+# brand[4].div.a.img["title"]
+# containers.findAll("li",{"class":"price-ship"})
+i = 1;
+
+for container in containers:
+	brand = container.img["title"]
+	print(i)
+	print("Brand: " + brand)
+	i+=1
