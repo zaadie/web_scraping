d05754143054d5eb217f44adf07fb666e05cd6ed
zaadie <saadiyafourie@gmail.com>
2019-04-06 19:37:40 +1100

    ...

--- redditcomments.py
+++ redditcomments.py
@@ -1,14 +1,19 @@
 import praw
 
 reddit = praw.Reddit(client_id='kTtEDzfVknEtUQ',
-                     client_secret='x',
-                     username='prawtutorial',
-                     password='cookies!!',
+                     client_secret='6DAtdBN1HJbRnRZAxXvUikyaKKU',
+                     username='CyberZii',
+                     password='Sha25694!!',
                      user_agent='prawtutorialv1')
 
 subreddit = reddit.subreddit('python')
+subreddit = reddit.subreddit('programmerhumor')
 
-hot_python = subreddit.hot(limit=5)
+hot_python = subreddit.hot(programmerhumor=5)
+top_progammerhumor = subreddit.top(limit=5)
 
-for submission in hot_python:
+# for submission in hot_python:
+#     print(submission.title)
+
+for submission in top_programmerhumor:
     print(submission.title)
